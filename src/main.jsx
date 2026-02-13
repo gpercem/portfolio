f4377ecb0da1ff360f051399a8b244fce0150943
Gökhan Perçem <81044486+gpercem@users.noreply.github.com>
2026-02-13 13:10:29 +0300

    added bulut

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import './styles/cursors.css'
 import App from './App.jsx'
+import Bulut from '@auticlabs/bulut'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Bulut
+      projectId='4ae21bf0-face-4cf8-b1a8-1043cefc46f3'
+    />
     <BrowserRouter>
       <App />
     </BrowserRouter>
